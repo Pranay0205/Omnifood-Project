32fb5ca72d42ad71873172613fb3a28109969615
Arundhuti2000 <44167900+Arundhuti2000@users.noreply.github.com>
2019-04-19 15:24:18 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,18 +1,29 @@
 <?php
-$connection = mysql_connect("localhost", "root", ""); // Establishing connection with server..
-$db = mysql_select_db("college", $connection); // Selecting Database.
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully"; // Establishing connection with server..
+$db = mysqli_select_db($conn,"college");
 $name=$_POST['name1']; // Fetching Values from URL.
-$email=$_POST['email1'];
-$password= sha1($_POST['rpassword1']); // Password Encryption, If you like you can also leave sha1.
+$email=$_POST['email2'];
+$password= ($_POST['rpassword1']); // Password Encryption, If you like you can also leave sha1.
 // Check if e-mail address syntax is valid or not
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
 echo "Invalid Email.......";
 }else{
-$result = mysql_query("SELECT * FROM registration WHERE email='$email'");
-$data = mysql_num_rows($result);
+$result = mysqli_query($conn,"SELECT * FROM registration WHERE email='$email'");
+$data = mysqli_num_rows($result);
 if(($data)==0){
-$query = mysql_query("insert into registration(name, email, password) values ('$name', '$email', '$password')"); // Insert query
+$query = mysqli_query($conn,"insert into registration(email, name, password) values ('$email', '$name', '$password')"); // Insert query
 if($query){
 echo "You have Successfully Registered.....";
 }else
@@ -23,5 +34,5 @@ echo "Error....!!";
 echo "This email is already registered, Please try another email...";
 }
 }
-mysql_close ($connection);
+mysqli_close ($conn);
 ?>
