551235aa87ff758f33cc89f0419e05719fc8e497
Arundhuti2000 <44167900+Arundhuti2000@users.noreply.github.com>
2019-04-19 23:57:11 +0530

    Update loginj.js

--- loginj.js
+++ loginj.js
@@ -12,14 +12,14 @@ $(document).ready(function(){
     function(data) {
     if(data=='Invalid Email.......') {
     $('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-    $('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
+    $('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 3px red"});
     alert(data);
     }else if(data=='Email or Password is wrong...!!!!'){
     $('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
     alert(data);
     } else if(data=='Successfully Logged in...'){
     $("form")[0].reset();
-    $('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
+    //$('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #3FBF3F"});
     alert(data);
     } else{
     alert(data);
